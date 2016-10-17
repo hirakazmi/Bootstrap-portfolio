5c58c2e0b9632cf67f92310f69b1b2ee0ae086f3
Hira Kazmi <hirarukh@gmail.com>
2016-10-16 21:22:23 -0400

    work page layout modified

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function() {
     $("#owl-demo").owlCarousel({
- 
+
       // navigation : true, // Show next and prev buttons
           slideSpeed : 300,
           paginationSpeed : 400,
@@ -11,14 +11,61 @@ $(document).ready(function() {
           items: 1,
           navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
           dots: false
-     
+
       // "singleItem:true" is a shortcut for:
-      // items : 1, 
+      // items : 1,
       // itemsDesktop : false,
       // itemsDesktopSmall : false,
       // itemsTablet: false,
       // itemsMobile : false
- 
+
   });
- 
+
+  $("#owl-demo2").owlCarousel({
+
+    // navigation : true, // Show next and prev buttons
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true,
+        loop:true,
+        nav:true,
+        items: 1,
+        navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+        dots: false
+
+    // "singleItem:true" is a shortcut for:
+    // items : 1,
+    // itemsDesktop : false,
+    // itemsDesktopSmall : false,
+    // itemsTablet: false,
+    // itemsMobile : false
+
+});
+
+//   // Get the modal
+// var modal = document.getElementById('myModal');
+//
+// // Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
+//
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+//
+// // When the user clicks on the button, open the modal
+// btn.onclick = function() {
+//     modal.style.display = "block";
+// }
+//
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//     modal.style.display = "none";
+// }
+//
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
+
 });
